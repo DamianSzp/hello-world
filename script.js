2ed8edea269830bd3b4eb0bb7698d1f91895e988
Damian Szpatusko <damian.szpatusko@circlekeurope.com>
2024-08-20 17:19:36 +0200

    Added decorative images between sections

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-// JavaScript for smooth page transitions
-
-document.querySelectorAll('a').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(event) {
-        if (this.href !== window.location.href) {
-            event.preventDefault();
-            document.body.classList.add('fade-out');
-            setTimeout(() => {
-                window.location.href = this.href;
-            }, 500);
-        }
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-});
-
-window.addEventListener('pageshow', (event) => {
-    if (event.persisted) {
-        document.body.classList.remove('fade-out');
-    }
 });
